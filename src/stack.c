c32e9f4c687a3f0832ba3750f0398efaea9ce323
Adonan17 <adnaneouassari@gmail.com>
2026-01-10 14:02:28 +0100

    creating stack file and adding stack_new function

---
+++ src/stack.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aouassar <aouassar@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/10 13:48:24 by aouassar          #+#    #+#             */
+/*   Updated: 2026/01/10 14:01:32 by aouassar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_stack	*stack_new(void)
+{
+	t_stack	*stack;
+
+	stack = malloc(sizeof(t_stack));
+	if (!stack)
+		return (NULL);
+	stack->top = NULL;
+	stack->bottom = NULL;
+	stack->size = 0;
+	return (stack);
+}
