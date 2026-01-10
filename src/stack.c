d428ff503098e85d0d3ce98ffc0d525941aa0d26
Adonan17 <adnaneouassari@gmail.com>
2026-01-10 14:09:59 +0100

    adding the node_new function to stack.c

--- src/stack.c
+++ src/stack.c
@@ -6,7 +6,7 @@
 /*   By: aouassar <aouassar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/10 13:48:24 by aouassar          #+#    #+#             */
-/*   Updated: 2026/01/10 14:01:32 by aouassar         ###   ########.fr       */
+/*   Updated: 2026/01/10 14:05:02 by aouassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,17 @@ t_stack	*stack_new(void)
 	stack->bottom = NULL;
 	stack->size = 0;
 	return (stack);
+}
+
+t_node	*node_new(int value)
+{
+	t_node	*node;
+
+	node = malloc(sizeof(t_node));
+	if (!node)
+		return (NULL);
+	node->value = value;
+	node->next = NULL;
+	node->prev = NULL;
+	return (node);
 }
