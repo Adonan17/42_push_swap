7f75cc5b58c0872c17b030f37572ad7db9529e5c
Adonan17 <adnaneouassari@gmail.com>
2026-01-12 13:46:58 +0100

    adding stack_swap_top function to stack_ops.c

--- src/stack_ops.c
+++ src/stack_ops.c
@@ -6,7 +6,30 @@
 /*   By: aouassar <aouassar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 12:20:50 by aouassar          #+#    #+#             */
-/*   Updated: 2026/01/12 12:20:51 by aouassar         ###   ########.fr       */
+/*   Updated: 2026/01/12 13:46:07 by aouassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+void	stack_swap_top(t_stack *stack)
+{
+	t_node	*firstnode;
+	t_node	*secondnode;
+	t_node	*thirdnode;
+
+	if (!stack || stack->size < 2)
+		return ;
+	firstnode = stack->top;
+	secondnode = firstnode->next;
+	thirdnode = secondnode->next;
+	stack->top = secondnode;
+	secondnode->prev = NULL;
+	secondnode->next = firstnode;
+	firstnode->prev = secondnode;
+	firstnode->next = thirdnode;
+	if (thirdnode)
+		thirdnode->prev = firstnode;
+	else
+		stack->bottom = firstnode;
+}
