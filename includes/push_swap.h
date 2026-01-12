6e347f09c461c0be7ee00810b391f53ee05f766a
Adonan17 <adnaneouassari@gmail.com>
2026-01-12 15:22:15 +0100

    updating header file

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: aouassar <aouassar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/10 13:30:55 by aouassar          #+#    #+#             */
-/*   Updated: 2026/01/10 13:42:27 by aouassar         ###   ########.fr       */
+/*   Updated: 2026/01/12 15:21:34 by aouassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,5 +30,20 @@ typedef struct s_stack
 	t_node	*bottom;
 	int		size;
 }	t_stack;
+
+// stack_new
+t_stack	*stack_new(void);
+t_node	*node_new(int value);
+
+// stack_push_pop
+void	stack_push_top(t_stack *stack, t_node *node);
+void	stack_push_bottom(t_stack *stack, t_node *node);
+t_node	*stack_pop_top(t_stack *stack);
+t_node	*stack_pop_bottom(t_stack *stack);
+
+// stack_ops.c
+void	stack_swap_top(t_stack *stack);
+void	stack_rotate(t_stack *stack);
+void	stack_rev_rotate(t_stack *stack);
 
 #endif
