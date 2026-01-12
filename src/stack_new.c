e34e6bd9355ba01a0342cb0d33bd938ae1d28e7a
Adonan17 <adnaneouassari@gmail.com>
2026-01-12 12:05:42 +0100

    renaming stack.c and creating different files for functions (more than 5)

---
+++ src/stack_new.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_new.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aouassar <aouassar@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/10 13:48:24 by aouassar          #+#    #+#             */
+/*   Updated: 2026/01/12 12:03:59 by aouassar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_stack	*stack_new(void)
+{
+	t_stack	*stack;
+
+	stack = malloc(sizeof(t_stack));
+	if (!stack)
+		return (NULL);
+	stack->top = NULL;
+	stack->bottom = NULL;
+	stack->size = 0;
+	return (stack);
+}
+
+t_node	*node_new(int value)
+{
+	t_node	*node;
+
+	node = malloc(sizeof(t_node));
+	if (!node)
+		return (NULL);
+	node->value = value;
+	node->next = NULL;
+	node->prev = NULL;
+	return (node);
+}
--- src/stack_push_pop.c
+++ src/stack_push_pop.c
@@ -1,42 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   stack.c                                            :+:      :+:    :+:   */
+/*   stack_push_pop.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aouassar <aouassar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/10 13:48:24 by aouassar          #+#    #+#             */
-/*   Updated: 2026/01/12 11:57:21 by aouassar         ###   ########.fr       */
+/*   Created: 2026/01/12 12:03:32 by aouassar          #+#    #+#             */
+/*   Updated: 2026/01/12 12:04:20 by aouassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-t_stack	*stack_new(void)
-{
-	t_stack	*stack;
-
-	stack = malloc(sizeof(t_stack));
-	if (!stack)
-		return (NULL);
-	stack->top = NULL;
-	stack->bottom = NULL;
-	stack->size = 0;
-	return (stack);
-}
-
-t_node	*node_new(int value)
-{
-	t_node	*node;
-
-	node = malloc(sizeof(t_node));
-	if (!node)
-		return (NULL);
-	node->value = value;
-	node->next = NULL;
-	node->prev = NULL;
-	return (node);
-}
 
 void	stack_push_top(t_stack *stack, t_node *node)
 {
