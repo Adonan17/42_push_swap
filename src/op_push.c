697b568fe16893f96b16854616a5542f428ed734
Adonan17 <adnaneouassari@gmail.com>
2026-01-14 12:11:41 +0100

    renaming stack_swap_rotatre.c and adding pb function to op_push.c

--- src/op_push.c
+++ src/op_push.c
@@ -6,7 +6,19 @@
 /*   By: aouassar <aouassar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 11:55:53 by aouassar          #+#    #+#             */
-/*   Updated: 2026/01/14 11:55:54 by aouassar         ###   ########.fr       */
+/*   Updated: 2026/01/14 12:10:50 by aouassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+void	pb(t_stack *a, t_stack *b)
+{
+	t_node	*node;
+
+	if (!a || !b || a->size == 0)
+		return ;
+	node = stack_pop_top(a);
+	stack_push_top(b, node);
+	write(1, "pb\n", 3);
+}
