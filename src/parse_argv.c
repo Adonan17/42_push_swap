9624385a1c04b3bae61b5fe70ae3fce05e70d691
Adonan17 <adnaneouassari@gmail.com>
2026-01-18 15:52:43 +0100

    finishing pars_argv.c file adding the needed helpers

--- src/parse_argv.c
+++ src/parse_argv.c
@@ -6,7 +6,7 @@
 /*   By: aouassar <aouassar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/16 21:10:58 by aouassar          #+#    #+#             */
-/*   Updated: 2026/01/16 21:49:52 by aouassar         ###   ########.fr       */
+/*   Updated: 2026/01/18 15:51:45 by aouassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 static void	free_split(char **tokens)
 {
 	int	i;
-	if (tokens == NULL)
+
+	if (!tokens)
 		return ;
 	i = 0;
 	while (tokens[i])
@@ -26,33 +27,58 @@ static void	free_split(char **tokens)
 	free(tokens);
 }
 
+static void	clear_stack(t_stack *a)
+{
+	t_node	*cur;
+	t_node	*next;
+
+	if (!a)
+		return ;
+	cur = a->top;
+	while (cur)
+	{
+		next = cur->next;
+		free(cur);
+		cur = next;
+	}
+	a->top = NULL;
+	a->bottom = NULL;
+	a->size = 0;
+}
+
+static void	parse_error(t_stack *a, char **tokens)
+{
+	free_split(tokens);
+	clear_stack(a);
+	write(2, "Error\n", 6);
+	exit(1);
+}
+
 void	parse_argv(int argc, char **argv, t_stack *a)
 {
 	char	**tokens;
 	int		i;
 	int		j;
+	int		err;
+	t_node	*node;
 
-	(void)a;
 	if (argc <= 1)
 		return ;
-	i = 1;
-	while (i < argc)
+	i = 0;
+	while (++i < argc)
 	{
 		tokens = ft_split(argv[i], ' ');
-		if (tokens == NULL || tokens[0] == NULL)
-		{
-			write(2, "Error\n", 5);
-			free_split(tokens);
-			exit(1);
-		}
+		if (!tokens || !tokens[0])
+			parse_error(a, tokens);
 		j = 0;
 		while (tokens[j])
 		{
-			parse_int(tokens[j], &err);
+			node = node_new(parse_int(tokens[j], &err));
+			if (err || !node)
+				parse_error(a, tokens);
+			stack_push_bottom(a, node);
 			j++;
 		}
 		free_split(tokens);
-		i++;
 	}
 }
-
