70dd0a546a2f07ef2a1261d1ac1919210a1dbf77
Adonan17 <adnaneouassari@gmail.com>
2026-01-19 20:04:14 +0100

    create a main.c file

---
+++ src/main.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aouassar <aouassar@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/19 15:44:01 by aouassar          #+#    #+#             */
+/*   Updated: 2026/01/19 20:03:33 by aouassar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	t_stack	*a;
+	t_stack	*b;
+	a = stack_new();
+	b = stack_new();
+	if (!a || !b)
+	{
+		clear_stack(a);
+		clear_stack(b);
+		free(a);
+		free(b);
+		write(2, "Error\n", 6);
+		return (1);
+	}
+	parse_argv(argc, argv, a);
+	clear_stack(a);
+	clear_stack(b);
+	free(a);
+	free(b);
+	return (0);
+}
