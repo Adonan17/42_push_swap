e89ab318005cc4c6c22b1d1c4c88e64f456e9ee0
Adonan17 <adnaneouassari@gmail.com>
2026-01-22 19:42:33 +0100

    finshing sort_five function in sort.c

--- src/sort.c
+++ src/sort.c
@@ -6,18 +6,34 @@
 /*   By: aouassar <aouassar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 12:05:09 by aouassar          #+#    #+#             */
-/*   Updated: 2026/01/22 19:11:57 by aouassar         ###   ########.fr       */
+/*   Updated: 2026/01/22 19:24:35 by aouassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	sort_two(t_stack *a)
+static void	push_min_to_b(t_stack *a, t_stack *b)
 {
-	if (!a || a->size != 2)
-		return ;
-	if (a->top->value > a->top->next->value)
-		sa(a);
+	int		min;
+	int		idx;
+	t_node	*cur;
+
+	min = get_min(a);
+	idx = 0;
+	cur = a->top;
+	while (cur && cur->value != min)
+	{
+		idx++;
+		cur = cur->next;
+	}
+	while (a->top && a->top->value != min)
+	{
+		if (idx <= (int)(a->size / 2))
+			ra(a);
+		else
+			rra(a);
+	}
+	pb(a, b);
 }
 
 void	sort_three(t_stack *a)
@@ -51,31 +67,10 @@ void	sort_three(t_stack *a)
 
 void	sort_five(t_stack *a, t_stack *b)
 {
-	int		min;
-	int		idx;
-	t_node	*cur;
-
 	if (!a || !b)
 		return ;
 	while (a->size > 3)
-	{
-		min = get_min(a);
-		idx = 0;
-		cur = a->top;
-		while (cur && cur->value != min)
-		{
-			idx++;
-			cur = cur->next;
-		}
-		while (a->top && a->top->value != min)
-		{
-			if (idx <= (int)(a->size / 2))
-				ra(a);
-			else
-				rra(a);
-		}
-		pb(a, b);
-	}
+		push_min_to_b(a, b);
 	sort_three(a);
 	while (b->size > 0)
 		pa(a, b);
@@ -86,7 +81,10 @@ void	sort(t_stack *a, t_stack *b)
 	if (!a || !b)
 		return ;
 	if (a->size == 2)
-		sort_two(a);
+	{
+		if (a->top->value > a->top->next->value)
+			sa(a);
+	}
 	else if (a->size == 3)
 		sort_three(a);
 	else if (a->size <= 5)
