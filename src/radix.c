328dcad4a2a2d7d8eef4887777468338e4cef3ae
Adonan17 <adnaneouassari@gmail.com>
2026-01-24 18:30:54 +0100

    adding radix_sort function to radix.c

--- src/radix.c
+++ src/radix.c
@@ -6,17 +6,17 @@
 /*   By: aouassar <aouassar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 16:56:03 by aouassar          #+#    #+#             */
-/*   Updated: 2026/01/24 17:02:37 by aouassar         ###   ########.fr       */
+/*   Updated: 2026/01/24 17:53:56 by aouassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	bit_counter(t_stack *a)
+static int	bits_counter(t_stack *a)
 {
 	int	bits;
 	int	max;
-	
+
 	max = a->size - 1;
 	bits = 0;
 	while (max > 0)
@@ -25,4 +25,30 @@ static int	bit_counter(t_stack *a)
 		bits++;
 	}
 	return (bits);
+}
+
+void	radix_sort(t_stack *a, t_stack *b)
+{
+	int	bit;
+	int	max_bits;
+	int	count;
+
+	if (!a || !b)
+		return ;
+	max_bits = bits_counter(a);
+	bit = 0;
+	while (bit < max_bits)
+	{
+		count = a->size;
+		while (count-- > 0)
+		{
+			if (((a->top->index >> bit) & 1) == 0)
+				pb(a, b);
+			else
+				ra(a);
+		}
+		while (b->size > 0)
+			pa(a, b);
+		bit++;
+	}
 }
